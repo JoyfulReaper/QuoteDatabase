d827f0b8ce2ad1491b5811c6e7a01a42b11c8b3d
Kyle Givler <xvegan88x@gmail.com>
2024-01-10 12:13:04 -0500

    Add forms for adding quotes, needs to be wired up

--- Frontend/QuoteDb/src/app/app.routes.ts
+++ Frontend/QuoteDb/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddSongComponent } from './add-song/add-song.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
+import { AddPersonComponent } from './add-person/add-person.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: "add-song", component: AddSongComponent },
+                                { path: "add-book", component: AddBookComponent },
+                                { path: "add-movie", component: AddMovieComponent },
+                                { path: "add-person", component: AddPersonComponent },
+];
