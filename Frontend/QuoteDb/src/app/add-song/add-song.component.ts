98b8dff02370a5ae34058519e57b97af954e4f24
Kyle Givler <xvegan88x@gmail.com>
2024-01-10 13:45:44 -0500

    Rework the FormGroup

--- Frontend/QuoteDb/src/app/add-song/add-song.component.ts
+++ Frontend/QuoteDb/src/app/add-song/add-song.component.ts
@@ -15,23 +15,17 @@ export class AddSongComponent {
   constructor(@Inject(SongService) private service: SongService) {}
 
   songForm = new FormGroup({
-    title: new FormControl(''),
-    album: new FormControl(''),
-    artist: new FormControl(''),
-    track: new FormControl(''),
-    text: new FormControl(''),
+    title: new FormControl<string>(''),
+    album: new FormControl<string>(''),
+    artist: new FormControl<string | null>(''),
+    track: new FormControl<number | null>(0),
+    text: new FormControl<string>(''),
   });
 
   onSubmit() {
     console.log(this.songForm.value);
 
-    const songRequest = new SongRequest();
-    songRequest.title = this.songForm.value.title ?? "";
-    songRequest.album = this.songForm.value.album ?? "";
-    songRequest.artist = this.songForm.value.artist ?? "";
-    songRequest.track = 0;
-    songRequest.text = this.songForm.value.text ?? "";
-
+    const songRequest = this.songForm.value as SongRequest;
     this.service.createSong(songRequest);
   }
 
