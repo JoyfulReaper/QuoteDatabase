a856f23002008551789d0304e280221f1defe83f
Kyle Givler <xvegan88x@gmail.com>
2024-01-12 12:41:14 -0500

    Add form validation to book form

--- Frontend/QuoteDb/src/app/add-book/add-book.component.ts
+++ Frontend/QuoteDb/src/app/add-book/add-book.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { BookRequest } from '../models';
 import { BookService } from '../book.service';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -19,22 +19,38 @@ export class AddBookComponent {
   showSaved = false;
 
   bookForm = new FormGroup({
-    title: new FormControl<string>(''),
-    text: new FormControl<string>(''),
-    author: new FormControl<string>(''),
+    title: new FormControl<string>('', [ Validators.required]),
+    text: new FormControl<string>('', [Validators.required]),
+    author: new FormControl<string>('', [Validators.required]),
     chapter: new FormControl<string | null>(''),
     page: new FormControl<number | null>(null),
   });
+
+  get title() {
+    return this.bookForm.get('title')!;
+  }
+
+  get text() {
+      return this.bookForm.get('text')!;
+    }
+
+  get author() {
+      return this.bookForm.get('author')!;
+    }
 
   async onSubmit() {
     console.log(this.bookForm.value);
 
     const bookRequest = this.bookForm.value as BookRequest;
     try {
-      await this.service.createBook(bookRequest);
-      this.showSaved = true;
-      this.showError = false;
-      this.bookForm.reset();
+      if (this.bookForm.valid) {
+        await this.service.createBook(bookRequest);
+        this.showSaved = true;
+        this.showError = false;
+        this.bookForm.reset();
+      }
+      else
+        console.log('Form is invalid');
     } catch (ex) {
       console.log(ex);
       this.showError = true;
