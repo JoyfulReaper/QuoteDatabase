c41bbe18e975baa17e4685a72296fdf428a595b2
Kyle Givler <xvegan88x@gmail.com>
2024-01-15 12:07:46 -0500

    Link to quote details from main page

--- Frontend/QuoteDb/src/app/main-page/main-page.component.ts
+++ Frontend/QuoteDb/src/app/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject } from '@angular/core';
 import { QuoteService } from '../quote.service';
 import { Quote, QuoteType } from '../models';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
@@ -13,7 +14,8 @@ export class MainPageComponent {
   constructor(@Inject(QuoteService) private service: QuoteService) {}
 
   quote: Quote = new Quote();
-  type: string;
+  quoteType: string;
+  quoteLink: string;
 
   async ngOnInit() {
     await this.getRandomQuote();
@@ -21,6 +23,20 @@ export class MainPageComponent {
 
   async getRandomQuote() {
     this.quote = await this.service.getRandomQuote();
-    this.type = QuoteType[this.quote.quoteType];
+    this.quoteType = QuoteType[this.quote.quoteType];
+    switch (this.quote.quoteType) {
+      case QuoteType.Book:
+        this.quoteLink = `/quotes/books/`;
+        break;
+      case QuoteType.Movie:
+        this.quoteLink = `/quotes/movies/`;
+        break;
+      case QuoteType.Person:
+        this.quoteLink = `/quotes/people/`;
+        break;
+      case QuoteType.Song:
+        this.quoteLink = `/quotes/songs/`;
+        break;
+    }
   }
 }
